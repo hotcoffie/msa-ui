4ccc9928b24688d1d1c76e8bb6db6ebbdae23c3f
xieyu <hotcoffie@163.com>
2021-01-08 11:29:45 +0800

    第一个可用版本

---
+++ conf/conf.go
@@ -0,0 +1,50 @@
+package conf
+
+import (
+	"encoding/json"
+	"github.com/pkg/errors"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+var Users = make(map[string]string)
+
+func init() {
+	initUsers()
+}
+func initUsers() {
+	file, err := os.OpenFile("users.json", os.O_CREATE|os.O_RDWR, os.ModePerm)
+	if err != nil {
+		log.Panic("打开用户信息:", err)
+	}
+	defer file.Close()
+
+	bs, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Panic("读取用户信息:", err)
+	}
+	if bs != nil && len(bs) > 0 {
+		if err = json.Unmarshal(bs, &Users); err != nil {
+			log.Panic("解析用户信息:", err)
+		}
+	}
+}
+
+func UpdateUser(username, password string) error {
+	p, ok := Users[username]
+	if ok && p == password {
+		return nil
+	}
+	Users[username] = password
+	file, err := os.OpenFile("users.yml", os.O_CREATE|os.O_RDWR, os.ModePerm)
+	if err != nil {
+		log.Panic("打开用户信息:", err)
+	}
+	defer file.Close()
+	data, _ := json.Marshal(Users)
+	if _, err = file.Write(data); err != nil {
+		return errors.WithMessage(err, "修改用户信息")
+	}
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module msa-ui
+
+go 1.14
+
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/pkg/errors v0.9.1
+	golang.org/x/net v0.0.0-20201224014010-6772e930b67b
+)
